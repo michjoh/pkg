75e0747360bfb6d25112c5b48b9bf7a8367d1a4c
jonskapel <michjohan@gmail.com>
2019-01-28 12:42:51 +0100

    from upstream

--- test/test-79-npm/checklist.js
+++ test/test-79-npm/checklist.js
@@ -4,6 +4,411 @@
 'use strict';
 
 const table = {
+  "angular-bridge": {
+    "win32/x64/m57": "ok,0.5.0"
+  },
+  "any-promise": {
+    "win32/x64/m57": "ok,1.3.0"
+  },
+  "async": {
+    "win32/x64/m57": "ok,2.6.1"
+  },
+  "aws-sdk": {
+    "win32/x64/m57": "ok,2.389.0"
+  },
+  "babel-core": {
+    "win32/x64/m57": "ok,6.26.3"
+  },
+  "babel-core@4.7.16": {
+    "win32/x64/m57": "ok,4.7.16"
+  },
+  "batch": {
+    "win32/x64/m57": "ok,0.6.1"
+  },
+  "bcrypt": {
+    "win32/x64/m57": "ok,3.0.3"
+  },
+  "bignum": {
+    "win32/x64/m57": "bad-npm-i,requires C:\\OpenSSL-Win64\\lib\\libeay32.lib"
+  },
+  "bindings": {
+    "win32/x64/m57": "ok,1.3.1"
+  },
+  "blessed": {
+    "win32/x64/m57": "ok,0.1.81"
+  },
+  "body-parser": {
+    "win32/x64/m57": "ok,1.18.3"
+  },
+  "body-parser@1.10.2": {
+    "win32/x64/m57": "ok,1.10.2"
+  },
+  "browserify": {
+    "win32/x64/m57": "ok,16.2.3"
+  },
+  "bson": {
+    "win32/x64/m57": "bad-test,4.0.1"
+  },
+  "bson@0.2.22": {
+    "win32/x64/m57": "ok,0.2.22"
+  },
+  "bson@0.4.0": {
+    "win32/x64/m57": "ok,0.4.0"
+  },
+  "bson@1.0.0": {
+    "win32/x64/m57": "ok,1.0.0"
+  },
+  "buffermaker": {
+    "win32/x64/m57": "ok,1.2.0"
+  },
+  "bunyan": {
+    "win32/x64/m57": "ok,1.8.12"
+  },
+  "busboy": {
+    "win32/x64/m57": "ok,0.3.0"
+  },
+  "bytes": {
+    "win32/x64/m57": "ok,3.0.0"
+  },
+  "bytes@2.5.0": {
+    "win32/x64/m57": "ok,2.5.0"
+  },
+  "callsites": {
+    "win32/x64/m57": "ok,3.0.0"
+  },
+  "chokidar": {
+    "win32/x64/m57": "ok,2.0.4"
+  },
+  "coffee-script": {
+    "win32/x64/m57": "ok,1.12.7"
+  },
+  "colors": {
+    "win32/x64/m57": "ok,1.3.3"
+  },
+  "compression": {
+    "win32/x64/m57": "ok,1.7.3"
+  },
+  "connect": {
+    "win32/x64/m57": "ok,3.6.6"
+  },
+  "connect-mongo": {
+    "win32/x64/m57": "ok,2.0.3"
+  },
+  "connect-mongo@0.8.2": {
+    "win32/x64/m57": "ok,0.8.2"
+  },
+  "connect-mongodb": {
+    "win32/x64/m57": "ok,1.1.5"
+  },
+  "connect-redis": {
+    "win32/x64/m57": "ok,3.4.0"
+  },
+  "connect-redis@2.5.1": {
+    "win32/x64/m57": "ok,2.5.1"
+  },
+  "connect@2.3.9": {
+    "win32/x64/m57": "ok,2.3.9"
+  },
+  "consolidate": {
+    "win32/x64/m57": "ok,0.15.1"
+  },
+  "cookie": {
+    "win32/x64/m57": "ok,0.3.1"
+  },
+  "cookie-parser": {
+    "win32/x64/m57": "ok,1.4.3"
+  },
+  "copy-paste": {
+    "win32/x64/m57": "ok,1.3.0"
+  },
+  "cors": {
+    "win32/x64/m57": "ok,2.8.5"
+  },
+  "cron": {
+    "win32/x64/m57": "ok,1.6.0"
+  },
+  "cross-env": {
+    "win32/x64/m57": "ok,5.2.0"
+  },
+  "cross-spawn-async": {
+    "win32/x64/m57": "ok,2.2.5"
+  },
+  "data-preflight": {
+    "win32/x64/m57": "nop"
+  },
+  "debug": {
+    "win32/x64/m57": "ok,4.1.1"
+  },
+  "denymount": {
+    "win32/x64/m57": "ok,2.2.0"
+  },
+  "diff": {
+    "win32/x64/m57": "ok,4.0.1"
+  },
+  "drivelist": {
+    "win32/x64/m57": "ok,6.4.6"
+  },
+  "drivelist@3.0.0": {
+    "win32/x64/m57": "ok,3.0.0"
+  },
+  "ed25519": {
+    "win32/x64/m57": "bad-npm-i,requires C:\\OpenSSL-Win64\\lib\\libeay32.lib"
+  },
+  "ejs": {
+    "win32/x64/m57": "ok,2.6.1"
+  },
+  "ejs@0.8.8": {
+    "win32/x64/m57": "ok,0.8.8"
+  },
+  "elasticsearch": {
+    "win32/x64/m57": "ok,15.3.0"
+  },
+  "electron": {
+    "win32/x64/m57": "ok,4.0.1"
+  },
+  "emailjs": {
+    "win32/x64/m57": "ok,2.2.0"
+  },
+  "engine.io": {
+    "win32/x64/m57": "ok,3.3.2"
+  },
+  "engine.io@1.8.0": {
+    "win32/x64/m57": "ok,1.8.0"
+  },
+  "epoll": {
+    "win32/x64/m57": "nop,only linux is supported"
+  },
+  "errorhandler": {
+    "win32/x64/m57": "ok,1.5.0"
+  },
+  "errors": {
+    "win32/x64/m57": "ok,0.3.0"
+  },
+  "eslint": {
+    "win32/x64/m57": "ok,5.12.0"
+  },
+  "etcher-image-write": {
+    "win32/x64/m57": "ok,9.1.4"
+  },
+  "exceljs": {
+    "win32/x64/m57": "ok,1.7.0"
+  },
+  "exiftool.exe": {
+    "win32/x64/m57": "ok,10.53.0"
+  },
+  "exiftool.pl": {
+    "win32/x64/m57": "ok,10.53.0"
+  },
+  "express": {
+    "win32/x64/m57": "ok,4.16.4"
+  },
+  "express-load": {
+    "win32/x64/m57": "ok,1.1.16"
+  },
+  "express-session": {
+    "win32/x64/m57": "ok,1.15.6"
+  },
+  "express-with-jade/express": {
+    "win32/x64/m57": "ok,4.16.4"
+  },
+  "express-with-jade/express@3.0.6": {
+    "win32/x64/m57": "ok,3.0.6"
+  },
+  "express-with-jade/express@3.21.2": {
+    "win32/x64/m57": "ok,3.21.2"
+  },
+  "express@3.5.3": {
+    "win32/x64/m57": "ok,3.5.3"
+  },
+  "extender": {
+    "win32/x64/m57": "ok,0.0.10"
+  },
+  "extsprintf": {
+    "win32/x64/m57": "ok,1.4.0"
+  },
+  "faye-websocket": {
+    "win32/x64/m57": "ok,0.11.1"
+  },
+  "feathers": {
+    "win32/x64/m57": "ok,2.2.4"
+  },
+  "findup-sync": {
+    "win32/x64/m57": "ok,3.0.0"
+  },
+  "floordate": {
+    "win32/x64/m57": "ok,3.0.0"
+  },
+  "fmt": {
+    "win32/x64/m57": "ok,1.1.0"
+  },
+  "formidable": {
+    "win32/x64/m57": "ok,1.2.1"
+  },
+  "fs-extra": {
+    "win32/x64/m57": "ok,7.0.1"
+  },
+  "fs-extra@0.20.1": {
+    "win32/x64/m57": "ok,0.20.1"
+  },
+  "fsevents": {
+    "win32/x64/m57": "nop,only macos is supported"
+  },
+  "geoip-lite": {
+    "win32/x64/m57": "ok,1.3.5"
+  },
+  "github": {
+    "win32/x64/m57": "bad-test,14.0.0"
+  },
+  "gm": {
+    "win32/x64/m57": "bad-test,1.23.1,requires GraphicsMagick/ImageMagick installed"
+  },
+  "google-closure-compiler": {
+    "win32/x64/m57": "ok,20181210.0.0"
+  },
+  "google-closure-compiler-java": {
+    "win32/x64/m57": "ok,20190106.0.0"
+  },
+  "google-closure-compiler@20180610.0.2": {
+    "win32/x64/m57": "ok,20180610.0.2"
+  },
+  "googleapis": {
+    "win32/x64/m57": "bad-test,36.0.0"
+  },
+  "got": {
+    "win32/x64/m57": "ok,9.6.0"
+  },
+  "graceful-fs": {
+    "win32/x64/m57": "ok,4.1.15"
+  },
+  "graceful-fs@3.0.8": {
+    "win32/x64/m57": "ok,3.0.8"
+  },
+  "grpc": {
+    "win32/x64/m57": "ok,1.18.0"
+  },
+  "gulp": {
+    "win32/x64/m57": "bad-test,4.0.0"
+  },
+  "hap-nodejs": {
+    "win32/x64/m57": "bad-npm-i,requires OpenSSL-dev and mdns installed"
+  },
+  "heapdump": {
+    "win32/x64/m57": "error,0.3.12"
+  },
+  "hoek": {
+    "win32/x64/m57": "ok,6.1.2"
+  },
+  "hoek@0.9.1": {
+    "win32/x64/m57": "ok,0.9.1"
+  },
+  "homebridge": {
+    "win32/x64/m57": "bad-npm-i,requires OpenSSL-dev and mdns installed"
+  },
+  "http-proxy": {
+    "win32/x64/m57": "ok,1.17.0"
+  },
+  "http-server": {
+    "win32/x64/m57": "ok,0.11.1"
+  },
+  "image-size": {
+    "win32/x64/m57": "ok,0.7.1"
+  },
+  "indexof": {
+    "win32/x64/m57": "ok,0.0.1"
+  },
+  "inquirer": {
+    "win32/x64/m57": "ok,6.2.1"
+  },
+  "j": {
+    "win32/x64/m57": "ok,1.0.0"
+  },
+  "jade": {
+    "win32/x64/m57": "ok,1.11.0"
+  },
+  "jsdom": {
+    "win32/x64/m57": "ok,13.1.0"
+  },
+  "jsdom@8.5.0": {
+    "win32/x64/m57": "ok,8.5.0"
+  },
+  "json-stringify-date": {
+    "win32/x64/m57": "ok,0.1.8"
+  },
+  "json-stringify-safe": {
+    "win32/x64/m57": "ok,5.0.1"
+  },
+  "json-stringify-safe@4.0.0": {
+    "win32/x64/m57": "ok,4.0.0"
+  },
+  "jsonwebtoken": {
+    "win32/x64/m57": "ok,8.4.0"
+  },
+  "kerberos": {
+    "win32/x64/m57": "bad-test,1.1.2"
+  },
+  "kerberos@0.0.10": {
+    "win32/x64/m57": "ok,0.0.10"
+  },
+  "knex": {
+    "win32/x64/m57": "ok,0.16.3"
+  },
+  "later": {
+    "win32/x64/m57": "ok,1.2.0"
+  },
+  "level": {
+    "win32/x64/m57": "ok,4.0.0"
+  },
+  "leveldown": {
+    "win32/x64/m57": "ok,4.0.1"
+  },
+  "levelup": {
+    "win32/x64/m57": "bad-test,4.0.0"
+  },
+  "liftoff": {
+    "win32/x64/m57": "ok,2.5.0"
+  },
+  "lodash": {
+    "win32/x64/m57": "ok,4.17.11"
+  },
+  "log4js": {
+    "win32/x64/m57": "ok,3.0.6"
+  },
+  "log4js@0.5.8": {
+    "win32/x64/m57": "ok,0.5.8"
+  },
+  "log4js@0.6.34": {
+    "win32/x64/m57": "ok,0.6.34"
+  },
+  "log4js@1.1.1": {
+    "win32/x64/m57": "ok,1.1.1"
+  },
+  "logform": {
+    "win32/x64/m57": "ok,2.1.0"
+  },
+  "machinepack-urls": {
+    "win32/x64/m57": "ok,6.0.1"
+  },
+  "machinepack-urls@5.0.0": {
+    "win32/x64/m57": "ok,5.0.0"
+  },
+  "markdown": {
+    "win32/x64/m57": "ok,0.5.0"
+  }
 };
 const changes = [
+  "bignum,win32/x64/m57: new bad-npm-i,requires C:\\OpenSSL-Win64\\lib\\libeay32.lib",
+  "bson,win32/x64/m57: new bad-test",
+  "data-preflight,win32/x64/m57: new nop",
+  "ed25519,win32/x64/m57: new bad-npm-i,requires C:\\OpenSSL-Win64\\lib\\libeay32.lib",
+  "epoll,win32/x64/m57: new nop,only linux is supported",
+  "fsevents,win32/x64/m57: new nop,only macos is supported",
+  "github,win32/x64/m57: new bad-test",
+  "gm,win32/x64/m57: new bad-test,requires GraphicsMagick/ImageMagick installed",
+  "googleapis,win32/x64/m57: new bad-test",
+  "gulp,win32/x64/m57: new bad-test",
+  "hap-nodejs,win32/x64/m57: new bad-npm-i,requires OpenSSL-dev and mdns installed",
+  "heapdump,win32/x64/m57: new error",
+  "homebridge,win32/x64/m57: new bad-npm-i,requires OpenSSL-dev and mdns installed",
+  "kerberos,win32/x64/m57: new bad-test",
+  "levelup,win32/x64/m57: new bad-test"
 ];
